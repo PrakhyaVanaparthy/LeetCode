9fdd702e52a09251432bb0656e227a6659cbf928
Prakhya Vanaparthy <prakhya_vanaparthy@yahoo.com>
2021-05-11 13:47:56 -0400

    Add files via upload
    This code is to acheive the following :
    
    1. To filter columns from an excel
    2. To copy cells from one excel to another

---
+++ copycolumn.py
@@ -0,0 +1,35 @@
+# Load Pandas and openpyxl
+import pandas as pd
+from openpyxl import load_workbook
+
+df = pd.read_excel('Wave.xlsx')
+# Reduce dataframe to target columns (by filtering on column names)
+df = df[['fruit', 'days']]
+
+#Copying the df output to excel
+out = pd.ExcelWriter('Book1.xlsx')
+df.to_excel(out, index=False)
+out.save()
+print('1')
+
+#Loading the required Worksheets
+src_wb = load_workbook('Book1.xlsx')
+dest_wb = load_workbook('Blueprints_Input_Sample.xlsx')
+src_sheet = src_wb.get_sheet_by_name('Sheet1')
+dest_sheet = dest_wb.get_sheet_by_name('Blueprints_Input_Sample')
+
+#Copying data from Src excel to required excel
+for i in range(1, src_sheet.max_row+1):
+    for j in range(1, src_sheet.max_column+1):
+        dest_sheet.cell(row=i+1, column=j+2).value = src_sheet.cell(row=i+1, column=j).value
+
+src_wb.save('source.xlsx')
+dest_wb.save('destination.xlsx')
+
+
+
+
+
+
+
+
